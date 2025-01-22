fe5f037371cb9da0d4b3b15031e5e3a5e2d44131
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-01-22 23:18:03 +0100

    refactor dashboard components to use app-dashboard-item with content projection <ng-content/>

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-item.component.html',
   styleUrl: './dashboard-item.component.css',
 })
-export class DashboardItemComponent {}
+export class DashboardItemComponent {
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
+
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
+}
