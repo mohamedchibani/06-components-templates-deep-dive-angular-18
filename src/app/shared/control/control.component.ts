efd1fa1241731435603d9af1d97d61324b27dc98
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-01-23 01:16:17 +0100

    refactor control component to enhance event handling and improve host binding

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +14,20 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control',
+    class: 'control', // new
+    '(click)': 'onClick()', // new & recommanded (listen to event on host element)
   },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';  // old (usage compatibility)
+
+  // old
+  /*  @HostListener('click') onClick(){ 
+    console.log('Clicked');
+  } */
   label = input.required<string>();
+
+  onClick() {
+    console.log('Clicked');
+  }
 }
