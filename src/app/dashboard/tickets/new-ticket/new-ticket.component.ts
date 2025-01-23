eed53abb9cc3873040d9cfa99b94a42af6eb53dc
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-01-23 11:21:43 +0100

    refactor new ticket component to reset form after submission by using ViewChild ElementRef

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -6,14 +6,17 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent, ControlComponent, FormsModule],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
     console.log('SUBMITTED!');
+    this.form?.nativeElement.reset();
   }
 }
